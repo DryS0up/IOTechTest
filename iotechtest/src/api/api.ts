defbc5c3cacceda108beb98f6dd0f2086824242f
SoupLord <116056629+DryS0up@users.noreply.github.com>
2025-02-27 11:59:40 +0400

    feat: add a hook for fetching items in api.ts

--- iotechtest/src/api/api.ts
+++ iotechtest/src/api/api.ts
@@ -1,1 +1,31 @@
+import { useState, useEffect} from "react"
 import {Items} from "../types"
+
+const API_URL = "https://jsonplaceholder.typicode.com/posts"
+
+export const useItems = () => {
+    const [items, setitems] = useState<Items[]>([])
+    const [error, seterror] = useState<string | null>(null)
+    const [loading, setloading] = useState<boolean>(true)
+
+    useEffect(() => {
+      const fetchItems = async () => {
+        try {
+            const response = await fetch(API_URL)
+            if(!response.ok){
+                throw new Error("Fetch Items Failed.")
+            }
+            const data:Items[] = await response.json()
+            setitems(data)
+        } catch (error: any) {
+            seterror(error.message || 'Error fetching Items')
+        }finally{
+            setloading(false)
+        }
+      }
+      fetchItems()
+      }, [])
+    return{items,error,loading,setitems}
+
+}
+  

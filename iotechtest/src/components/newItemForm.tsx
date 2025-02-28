70d51381d357148067913b74a7bbaf33f9d6629d
SoupLord <116056629+DryS0up@users.noreply.github.com>
2025-02-28 13:53:50 +0400

    style: add styling to create-item form menu

--- iotechtest/src/components/newItemForm.tsx
+++ iotechtest/src/components/newItemForm.tsx
@@ -30,11 +30,11 @@ export function NewItemForm(props:newItemFormProp){
     }
 
     return(
-        <form onSubmit={submitHandler}>
-            {error && <p>{error}</p>}
-            <input type="text" placeholder="title" value={title} onChange={i => setTitle(i.target.value)}/>
-            <textarea placeholder="Description" value={body} onChange={i => setbody(i.target.value)}></textarea>
-            <button type="submit">Add Item</button>
+        <form onSubmit={submitHandler} className="pr-4 pb-4 mb-4 flex-colp-4 border rounded shadow flex flex-col space-y-4 bg-gray-300 w-1/3">
+            {error && <p className="text-red-500">{error}</p>}
+            <input type="text" placeholder="title" value={title} onChange={i => setTitle(i.target.value)} className="w-full p-4 m-2 rounded"/>
+            <textarea placeholder="Description" value={body} onChange={i => setbody(i.target.value)} className="w-full p-4 m-2 rounded"></textarea>
+            <button type="submit" className="p-4 border bg-gray-700 mx-2 rounded w-1/4 text-white">Add Item</button>
         </form>
     )
 }
